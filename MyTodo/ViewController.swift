fc009567a4ccd147c2c735b2ebc287214a883c76
sventouz <sventouz@gmail.com>
2019-12-25 19:09:35 +0900

    Initial Commit

---
+++ MyTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodo
+//
+//  Created by higuchiryunosuke on 2019/12/25.
+//  Copyright © 2019年 higuchiryunosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
