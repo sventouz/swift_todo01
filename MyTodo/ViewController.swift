ccf64e983ebbbd550ce7edf3bcb84e899b4aa411
sventouz <sventouz@gmail.com>
2019-12-26 20:19:03 +0900

    initial commit

--- MyTodo/ViewController.swift
+++ MyTodo/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let todo = ["牛乳を買う", "髪を切る", "掃除をする"]
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //セルに表示する値を取得する
+        cell.textLabel!.text = todo[indexPath.row]
+        return cell
     }
 
 
